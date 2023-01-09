8ab02c1eb05ed5d86ec78caccc3e8d5553dc6e57
Moraes-Bruno <moraesbruno888@gmail.com>
2023-01-09 13:54:06 -0300

    Projetos movidos da branch master para a main

---
+++ ProjetoAcampamento/src/AcampamentoTeste.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class AcampamentoTeste {
+    public static void main(String[] args){
+    Acampamento membro = new Acampamento();
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Digite o nome do escoteiro: ");
+        membro.setNome(sc.nextLine());
+        System.out.println("Digite a idade do escoteiro: ");
+        membro.setIdade(Integer.parseInt(sc.nextLine()));
+
+        membro.imprimir();
+        membro.separargrupo();
+        membro.imprimir();
+
+
+    }
+}
